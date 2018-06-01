d5e90321fb92bd936eb8eb7e8f84f6b244ea2e6d
wangbin <514330014@qq.com>
2018-06-01 14:18:31 +0800

    conf

---
+++ Conf/config.php
@@ -0,0 +1,11 @@
+<?php 
+	$config = array(
+	'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'localhost',
+	'DB_NAME'=>'youyjia',
+	'DB_USER'=>'youyijia',
+	'DB_PWD'=>'R3J3X4P5',
+	'DB_PORT'=>'3306',
+	'DB_PREFIX'=>'joys_',);
+	//return array_merge(include './Conf/config.php',$config);
+?>
